fdc6e8ed5a926ed35dd3cd64b649ae036612b0fa
nastasia01 <72750240+nastasia01@users.noreply.github.com>
2020-11-24 22:07:10 +0700

    Add files via upload

---
+++ Shurubey_laba1_oop.cpp
@@ -0,0 +1,15 @@
+﻿#include "Date_time.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	//Date_time obj;
+	//cout << obj.to_String() << endl;
+	Date_time primer1;
+	primer1.primer1();
+	Date_time primer2;
+	primer2.primer2();
+
+}
